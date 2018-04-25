7cd5bb20bab8d19c7b53b883df3376643a446507
wang <wshxbqq@gmail.com>
2018-04-25 18:39:30 +0800

    add

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-var ffmpeg = require('ffmpeg-static');
+$(".left-title").on("click", function (e) {
+    let $this = $(this);
+    $(".left-container").hide();
+    $this.next().show();
+})
+
+$(".left-block .left-container a").on("click", function (e) {
+    if (window.top.processing) {
+        e.preventDefault();
+        return;
+    }
+    let $this = $(this);
+    $(".sp-1").html($this.parent().parent().prev().text());
+    $(".sp-2").html($this.text());
+});
--- main.js
+++ main.js
@@ -59,5 +59,16 @@ app.on('activate', function () {
     }
 })
 
+const ipc = require('electron').ipcMain
+const dialog = require('electron').dialog
+
+ipc.on('open-file-dialog', function (event) {
+    dialog.showOpenDialog({
+        properties: ['openDirectory']
+    }, function (files) {
+        if (files) event.sender.send('selected-directory', files)
+    })
+})
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
