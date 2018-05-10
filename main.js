cacc1073e5e39b62599a623ea21d45de0e9c3f7d
wang <wshxbqq@gmail.com>
2018-05-10 18:34:13 +0800

    add

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow() {
         protocol: 'file:',
         slashes: true
     }))
-    //mainWindow.webContents.openDevTools()
+    mainWindow.webContents.openDevTools()
     mainWindow.on('closed', function () {
         mainWindow = null
     })
